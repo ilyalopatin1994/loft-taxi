423f9004a2b51bea173ad89bf6e3cb84243dd9d2
ilyalopatin1994 <shark132@yandex.ru>
2022-05-31 21:29:49 +0300

    fix(file directory): config.js moved to src/

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const mapItemName = 'Карта';
+export const registrationPage = 'Регистрация';
+export const loginItemName = 'Логин';
+export const profileItemName = 'Профиль';
+export const signOutItemName = 'Выйти';
